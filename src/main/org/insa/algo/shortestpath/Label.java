b78181b0acccbc7a07afacf99944f0ffc6daff08
M. Rollain Thomas <rollain@insa-08114>
2019-04-17 18:14:42 +0200

    classe Label

---
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -0,0 +1,50 @@
+package org.insa.algo.shortestpath;
+
+import org.insa.graph.Node;
+
+
+public class Label {
+	private Node currentNode;
+	private Node nodeFather;
+	private boolean mark;
+	private int cost;
+	
+	public Label(Node n) {
+		this.currentNode = n;
+		this.nodeFather = null;
+		this.mark = false;
+		
+	}
+	
+	public Node getNode() {
+		return this.currentNode;
+	}
+	
+	public int getCost() {
+		return this.cost;
+	}
+	
+	public Node getFatherNode() {
+		return this.nodeFather;
+	}
+
+	public void setMark() {
+		this.mark = true;
+	}
+	
+	/* Compare le coûts des labels entre eux */
+	public int compare(Label autre) {
+		int resultat;
+		if(this.getCost() < autre.getCost()) {
+			resultat = -1;
+		}
+		else if (this.getCost() == autre.getCost()) {
+			resultat = 0;
+		}
+		else {
+			resultat = 1; 
+		}
+		return resultat;
+	}
+	
+}
